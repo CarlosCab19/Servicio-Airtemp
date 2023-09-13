1a277c6b6e19bc39c4ec940f4137d012fc4a3455
CarlosCab19 <CabTunCarlos@gmail.com>
2023-09-13 14:29:28 -0600

    actualización 13/09
    Se modifico el formulario del la solicitud de cotización, se agrego el nombre del solicitante desde la tabla del personal

--- frontend/src/app/vista-usuario/form-solicitud-c/form-solicitud-c.component.ts
+++ frontend/src/app/vista-usuario/form-solicitud-c/form-solicitud-c.component.ts
@@ -9,9 +9,9 @@ import { Personal } from 'src/app/shared/personal';
   styleUrls: ['./form-solicitud-c.component.css']
 })
 export class FormSolicitudCComponent {
-  id!:number;
-  personal!:Personal;
+
   fechaActual: string;
+  selectedFile: File | null = null;
 
   constructor(private router: Router, public personalService: PersonalService) {
     // Obtener la fecha actual y formatearla como "YYYY-MM-DD"
@@ -20,6 +20,18 @@ export class FormSolicitudCComponent {
     const month = String(today.getMonth() + 1).padStart(2, '0');
     const day = String(today.getDate()).padStart(2, '0');
     this.fechaActual = `${year}-${month}-${day}`;
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0] as File;
+  }
+  onSubmit() {
+    if (this.selectedFile) {
+      // Aquí puedes enviar el archivo al servidor o realizar otras acciones con él.
+      // Por ejemplo, puedes usar una biblioteca como 'FormData' para enviarlo a un servidor.
+    } else {
+      alert('Por favor, seleccione un archivo PDF antes de enviarlo.');
+    }
   }
 
 
--- frontend/src/app/vista-usuario/vista-usuario.component.ts
+++ frontend/src/app/vista-usuario/vista-usuario.component.ts
@@ -10,27 +10,48 @@ import { Personal } from '../shared/personal';
 })
 export class VistaUsuarioComponent implements OnInit{
 
-  nombrePersonal:string="ffgdg";
+  fechaActual: string;
+  selectedFile: File | null = null;
+  nombrePersonal:string="";
+  apellidoPersonal:string="";
   id:string="";
   personalN:Personal | undefined;
+  mostrarInformacion: boolean = false;
+  mostrarSolicitudes:boolean = false;
+  mostrarForm:boolean = true;
 
 
   constructor(private router: Router,private rutaActiva: ActivatedRoute,
-              private personalService:PersonalService){}
+      private personalService:PersonalService){
+      // Obtener la fecha actual y formatearla como "YYYY-MM-DD"
+      const today = new Date();
+     const year = today.getFullYear();
+      const month = String(today.getMonth() + 1).padStart(2, '0');
+      const day = String(today.getDate()).padStart(2, '0');
+      this.fechaActual = `${year}-${month}-${day}`;}
 
-  ngOnInit(): void {
-    /*this.id=this.rutaActiva.snapshot.paramMap.get('id');
-    this.personService.find(this.id).subscribe(response=>{
-      this.personalN=response;
-      this.nombrePersonal=response.nombres;
-    });*/
-
+  onFileSelected(event: any) {
+      this.selectedFile = event.target.files[0] as File;
   }
 
-
-  mostrarInformacion: boolean = false;
+  ngOnInit(): void {
+    this.id=this.rutaActiva.snapshot.paramMap.get('id') as string;
+    this.personalService.find(this.id).subscribe(response=>{
+      //console.log(response); // Agrega esta línea para depurar
+      this.personalN=response;
+      this.nombrePersonal=response.nombres;
+      this.apellidoPersonal=response.apellidos;
+    });
+  }
 
   toggleInfo() {
     this.mostrarInformacion = !this.mostrarInformacion;
+  }
+  toggleSoli(){
+    this.mostrarSolicitudes;
+  }
+  toggleForm(){
+    this.mostrarSolicitudes = !this.mostrarSolicitudes;
+    this.mostrarForm = !this.mostrarForm;
   }
 }
